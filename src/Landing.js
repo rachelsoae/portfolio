cb244107c0d48efee0d5254746fd0a3698767532
Rachel Prather <rachelprather1@gmail.com>
2023-08-21 14:59:20 -0700

    Style landing page

---
+++ src/Landing.js
@@ -0,0 +1,16 @@
+const Landing = () => {
+  return (
+    <div className='page landing'>
+      <section className='landing__box'>
+        <div className='landing__text'>
+          <p>Hi, my name is</p>
+          <h1>Rachel Soae Prather</h1>
+          <p>I’m a Software Developer specializing in front end technologies including React, JavaScript, and Sass</p>
+          <button className='landing__button'>View My Work</button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Landing;
