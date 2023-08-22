6a68e1f683eecdf284f19f0db3576c744106e987
Rachel Prather <rachelprather1@gmail.com>
2023-08-21 17:57:20 -0700

    Style nav bar

--- src/Landing.js
+++ src/Landing.js
@@ -1,15 +1,20 @@
+import Nav from './Nav'
+
 const Landing = () => {
   return (
+    <>
+    <Nav />
     <div className='page landing'>
       <section className='landing__box'>
         <div className='landing__text'>
           <p>Hi, my name is</p>
-          <h1>Rachel Soae Prather</h1>
+          <h2>Rachel Soae Prather</h2>
           <p>I’m a Software Developer specializing in front end technologies including React, JavaScript, and Sass</p>
           <button className='landing__button'>View My Work</button>
         </div>
       </section>
     </div>
+    </>
   )
 }
 
