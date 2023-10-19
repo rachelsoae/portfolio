5f52891cc97ca07b07b5655e4920aa44f3ac1a40
Rachel Prather <rachelprather1@gmail.com>
2023-10-18 17:32:30 -0700

    Dynamically render tech stack icons

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,26 +1,27 @@
 import Project from '../Project/Project'
 
-
-// iterate over projects and generate Project component with that project's passed in as prop
-
 const Portfolio = () => {
   const PROJECTS = [
     {
+      id: 1,
       name: 'EverUse',
       tagline: 'This product-focused e-commerce app was built on behalf of the small sustainability business EverUse Upcycled Products.',
-      tech: ['React', 'JavaScript', 'Sass', 'Cypress', 'GraphQL', 'GitHub Actions']
+      techs: ['React', 'JavaScript', 'Sass', 'Cypress', 'GraphQL', 'GitHub Actions']
     },
     {
+      id: 2,
       name: 'D&D : The Rookie\'s Roadmap',
       tagline: 'A D&D Wiki for complete beginners. Start your journey here!',
-      tech: ['React', 'JavaScript', 'Sass', 'Cypress']
+      techs: ['React', 'JavaScript', 'Sass', 'Cypress']
     },
     {
+      id: 3,
       name: 'Humor Me',
       tagline: 'Need a lift? Enjoy clicking through randomized motivational posters, or try Chaotic Mode at your own risk.',
-      tech: ['React', 'JavaScript', 'CSS', 'Cypress', 'Express', 'Knex', 'PostgreSQL']
+      techs: ['React', 'JavaScript', 'CSS', 'Cypress', 'Express', 'Knex', 'PostgreSQL']
     }
     // overlook: {
+    //   id: 4,
     //   name: 'Overlook',
     //   tagline: 'Book your stay at the luxurious Overlook Hotel. Sign in for a personalized experience. Demo Username: customer30, Demo Password: overlook2021',
     //   tech: ['React', 'JavaScript', 'Sass/SCSS', 'Cypress']
@@ -31,7 +32,7 @@ const Portfolio = () => {
     <div className='projects-section'>
       <h2>Portfolio</h2>
       <section className='projects'>
-        {PROJECTS.map(project => <Project project={project} />)}
+        {PROJECTS.map(project => <Project key={project.id} project={project} />)}
       </section>
     </div>
   )
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,13 +1,44 @@
-// destructure argument in component and dynamically render the different parts
+import css from '../../assets/icons/css.png';
+import cypress from '../../assets/icons/cypress.png';
+import express from '../../assets/icons/express.png';
+import githubactions from '../../assets/icons/githubactions.png';
+import graphql from '../../assets/icons/graphql.png';
+import javascript from '../../assets/icons/javascript.png';
+import knex from '../../assets/icons/knex.png';
+import postgresql from '../../assets/icons/postgresql.png';
+import react from '../../assets/icons/react.png';
+import sass from '../../assets/icons/sass.png';
 
 const Project = ({project}) => {
-  const {name, tagline, tech} = project;
+  const {name, tagline, techs} = project;
+
+  const ICONS = {
+    'CSS': css,
+    'Cypress': cypress,
+    'Express': express,
+    'GitHub Actions': githubactions,
+    'GraphQL': graphql,
+    'JavaScript': javascript,
+    'Knex': knex,
+    'PostgreSQL': postgresql,
+    'Sass': sass,
+    'React': react, 
+  }
 
   return (
     <article>
       <h3>{name}</h3>
       <p>{tagline}</p>
-      {tech.map(t => <p>{t}</p>)}
+      <div className='tech'>
+        {techs.map(tech => {
+          return (
+            <div className='tech-icon-container'>
+              <img src={ICONS[tech]} alt={tech} />
+              <p>{tech}</p>   
+            </div>
+          )
+        })}
+      </div>
     </article>
   )
 }
