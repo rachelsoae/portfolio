c39e2fa553f0458f764e104027057da537783e8e
Rachel Prather <rachelprather1@gmail.com>
2023-11-20 17:20:41 -0800

    Set max-width to nav content

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,23 @@
-import {Link, useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import logo from '../../assets/logo.png'
 
 const Nav = () => {
   return (
     <nav>
-      <h1>
-        <Link to='/'>
-          <img src={logo} id='logo' alt='Rachel Soae, Software Engineer' />
-        </Link>
-      </h1>
-      <div className='nav__buttons'>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/portfolio'>Portfolio</Link>
-        <Link to='/contact'>
-          <button  className='primary-button'>Contact Me</button>
-        </Link>
+      <div className='nav__wrapper'>
+        <h1>
+          <Link to='/'>
+            <img src={logo} id='logo' alt='Rachel Soae Prather, Software Engineer' />
+          </Link>
+        </h1>
+        <div className='nav__buttons'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/portfolio'>Portfolio</Link>
+          <Link to='/contact'>
+            <button  className='primary-button'>Contact Me</button>
+          </Link>
+        </div>
       </div>
     </nav>
   )
