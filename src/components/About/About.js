fa52a15e0fa82dd605d091c05f0a2feb173ca9dc
Rachel Prather <rachelprather1@gmail.com>
2023-12-27 20:56:16 -0800

    Add media queries for About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,38 +3,32 @@ import {Link} from 'react-router-dom';
 
 const About = () => {
   return (
-    <div className='about-wrapper'>
-      <h2>About</h2>
-      <div className='about-content'>
+    <div className='about'>
+      <h2 className='page-header'>About</h2>
+      <div className='about__content'>
         <img 
+          className='about__image'
           src={headshot} 
-          alt='Rachel, a woman with glasses, long brown hair, and wearing a blue blazer, smiles warmly at the camera'
-          className='about-image'
+          alt='Rachel smiles warmly at the camera. She is wearing a blue blazer and tortoiseshell glasses.'
         />
-        <section className='about-text'>
+        <section className='about__text'>
           <p>
-            Hello! 👋 My name is Rachel Soae Prather. I'm a Korean-American Software Engineer and Web Developer located outside Portland, Oregon. 
-          </p>
-          <p> 
-            Born and raised in Northern California, I attended the University of Kansas (Rock Chalk!) in Lawrence, KS, and resided briefly in Fort Collins, CO, before finally relocating to the Pacific Northwest.
+            Hello! 👋 My name is Rachel Soae Prather. I'm a Korean-American Software Engineer and Web Developer located outside Portland, Oregon.
           </p>
           <p>
-            A resourceful, enthusiastic, and community-oriented technologist, I bring a breadth of knowledge to my work, gained through varied experience in veterinary medicine, nonprofit operations, education, and mortgage lending. 
+            My mission is to Build Technology that Builds Community. In service of that mission, I work and volunteer with ethical organizations whose missions I strongly believe in. I am currently associated with Rita XYZ, Lesbians Who Tech & Allies, Out In Tech, Girls Who Code, and PDXWIT.
           </p>
           <p>
-            I am dedicated to excellence and constant improvement and have earned multiple awards for outstanding performance and service... although my proudest achievement was being voted "Most Uplifting" by my graduating class at Turing School of Software & Design, where I earned a Certificate in Front End Software Engineering. 
+            In my downtime, I can be found eating kimchi, searching for the best honey at the farmer's market, and making music with friends.
           </p>
           <p>
-            My mission is to Build Technology that Builds Community. In service of that mission, I work and volunteer with ethical organizations that strive to better the world around them. I am currently associated with Rita XYZ, Lesbians Who Tech & Allies, and PDXWIT.
-          </p>
-          <p>
-            In my downtime, I can be found eating kimchi, hunting for the best honey at the farmer's market, and making music with friends.
-          </p>
-          <p>
-            I would love to connect with you. <Link to='/contact' id='contact-link'>Let's build something together.</Link>
+            I would love to connect with you. <Link to='/contact' id='contact-link'>Let's create something meaningful together.</Link>
           </p>
         </section>
       </div>
+      <section className='about__resume'>
+        <p>Virtual resume coming soon!</p>
+      </section>
     </div>
   )
 }
