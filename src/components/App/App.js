984386344baae3f650ddbbf78ef7b45daefd4c84
Rachel Prather <rachelprather1@gmail.com>
2023-12-28 17:42:43 -0800

    Create Menu component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+import Menu from '../Menu/Menu';
 import Nav from '../Nav/Nav';
 import Home from '../Home/Home';
 import About from '../About/About';
@@ -13,17 +14,7 @@ const App = () => {
   return (
     <div className='app'>
         {isOpen ?
-        <nav className='menu'>
-          <span class='material-symbols-rounded' id='menu__exit' onClick={() => setIsOpen(!isOpen)}>
-            close
-          </span>
-          <div className='top-nav__buttons menu__buttons'>
-            <Link to='/' onClick={() => setIsOpen(!isOpen)}className='menu__button'>Home</Link>
-            <Link to='/about' onClick={() => setIsOpen(!isOpen)} className='menu__button'>About</Link>
-            <Link to='/portfolio' onClick={() => setIsOpen(!isOpen)} className='menu__button'>Portfolio</Link>
-            <Link to='/contact' onClick={() => setIsOpen(!isOpen)} className='menu__button'>Contact</Link>
-          </div>
-        </nav>
+        <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
         :
         <>
           <Nav isOpen={isOpen} setIsOpen={setIsOpen} />
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom';
+
+const Menu = ({isOpen, setIsOpen}) => {
+  return (
+    <nav className='menu'>
+      <span class='material-symbols-rounded' id='menu__exit' onClick={() => setIsOpen(!isOpen)}>
+        close
+      </span>
+      <div className='top-nav__buttons menu__buttons'>
+        <Link to='/' onClick={() => setIsOpen(!isOpen)}className='menu__button'>Home</Link>
+        <Link to='/about' onClick={() => setIsOpen(!isOpen)} className='menu__button'>About</Link>
+        <Link to='/portfolio' onClick={() => setIsOpen(!isOpen)} className='menu__button'>Portfolio</Link>
+        <Link to='/contact' onClick={() => setIsOpen(!isOpen)} className='menu__button'>Contact</Link>
+      </div>
+  </nav>
+  )
+}
+
+export default Menu;
