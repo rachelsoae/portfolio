c15a1b6e424bf4c31acb5fa73154b7f35eb11965
Rachel Prather <rachelprather1@gmail.com>
2024-01-12 12:28:58 -0800

    Scroll to top on link click

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom';
 const TopNav = () => {
   return (
     <>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/portfolio'>Portfolio</Link>
-      <Link to='/contact'>
+      <Link to='/' onClick={() => {window.scroll(0, 0)}}>Home</Link>
+      <Link to='/about' onClick={() => {window.scroll(0, 0)}}>About</Link>
+      <Link to='/portfolio' onClick={() => {window.scroll(0, 0)}}>Portfolio</Link>
+      <Link to='/contact' onClick={() => {window.scroll(0, 0)}}>
         <button  className='primary-button'>Contact Me</button>
       </Link>
     </>
